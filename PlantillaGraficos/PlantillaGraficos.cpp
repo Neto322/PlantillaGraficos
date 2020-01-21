a935b4b4ff815c5320bd12602a95e33c8446216f
alumno <alumno@E302>
2020-01-21 08:49:07 -0700

    Dibujamos cosas con arreglos de vectores y colores.

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -12,6 +12,90 @@
 #include <iostream>
 
 using namespace std;
+
+
+void dibujarTrianguloContinuo() {
+	glBegin(GL_TRIANGLE_STRIP);
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.1f, 0.1f, 0.0f);
+	glVertex3f(0.2f, 0.0f, 0.0f);
+	glColor3f(1.0f, 0.5f, 1.0f);
+	glVertex3f(0.2f, 0.3f, 0.0f);
+
+	glEnd();
+}
+
+void dibujarPolygono() {
+	glBegin(GL_POLYGON);
+	glColor3f(0.4f, 0.4f, 0.9f);
+
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.5f, 0.0f);
+	glVertex3f(0.2f, 0.3f, 0.0f);
+	glVertex3f(0.6f, -0.4f, 0.0f);
+	glVertex3f(0.4f, - 0.6f, 0.0f);
+
+
+	glEnd();
+}
+void dibujarTriangulos() {
+	/*
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	*/
+	//Establecemos el tipo de primitiva
+	glBegin(GL_TRIANGLES);
+	//Establecemos Color
+	glColor3f(1.0f, 0.0f, 0.0f);
+	//Enviar vertices
+	glVertex3f(-0.8f, 0.8f, 0.0f);
+	glVertex3f(-0.8f, -0.8f, 0.0f);
+	glVertex3f(0.8f, -0.8f, 0.0f);
+
+	glVertex3f(0.8f, 0.8f, 0.0f);
+	glVertex3f(0.8f, -0.8f, 0.0f);
+	glVertex3f(-0.8f, 0.8f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.01f, 0.0f);
+	glVertex3f(-0.01f, -0.01f, 0.0f);
+	glVertex3f(0.01f, -0.01f, 0.0f);
+
+	//Especficar que dejaremos de dibujar.
+	glEnd();
+}
+
+void dibujarLineaContinua()
+{
+	glBegin(GL_LINE_STRIP);
+	glColor3f(0.2f, 0.3f, 1.0f);
+	glVertex3f(0.1f, 0.3f, 0.0f);
+	glColor3f(0.5f, 0.8f, 0.3f);
+	glVertex3f(0.1f, -0.3f, 0.0f);
+	glVertex3f(0.4f, -0.3f, 0.0f);
+
+	glEnd();
+}
+void dibujarLineas()
+{
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.4f, 0.6f);
+
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.2f, -0.4f, 0.0f);
+
+	glVertex3f(-0.3f, 0.1f, 0.0f);
+	glVertex3f(-3.0f,-0.1f,0.0f);
+	glEnd();
+
+}
+void dibujar() {
+	dibujarPolygono();
+}
+
 int main()
 {
 	//Declarar una ventana
@@ -27,7 +111,7 @@ int main()
 	//Si se puedo iniciar GLFW
 	//Inicializmos la ventana
 	//Todas las funciones especificas de GLFW tienen el sufijo glfw
-	window = glfwCreateWindow(800,600,"Ventana",NULL,NULL);
+	window = glfwCreateWindow(600,600,"Ventana",NULL,NULL);
 
 	//Si no pudo crear la ventana
 	//Terminamos ejecucion
@@ -59,13 +143,18 @@ int main()
 	while (!glfwWindowShouldClose(window))
 	{
 		//Establecer region de dibujo
-		glViewport(0, 0, 800, 600);
+		glViewport(0, 0, 600, 600);
 		//Establecemos el color de borrado.
 		glClearColor(1,0.8,0,1);
 		//Borrar!
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		//Actualizar valores y dibujar
+		dibujar();
+
+		glfwPollEvents();
+
 		glfwSwapBuffers(window);
+
 	}
 	//Despues del ciclo de dibujo
 	//Eliminamos venta y procesos de glfw
